38537b71b5aa5d3dde92f2a20299c228834de165
DatVTb <datvtb@bkav.com>
2014-12-20 11:49:12 +0700

    Update core.c

--- core.c
+++ core.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <string.h>	//A
-
-#define MAX 100	//A
 
 int main(int argc, char ** argv)
 {
-  char name[MAX];	//A
-  printf("Enter your name: \n");	//A
-  fflush(stdin);	//A
-  gets(name);	//A
+  printf("Hello World\n");
   return 0;
 }
