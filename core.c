f421769563441e68f2165dd49da4f6d425c29986
DatVTb <datvtb@bkav.com>
2014-12-20 12:18:06 +0700

    Update core.c

--- core.c
+++ core.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
-#include <string.h>	//B
-#include <conio.h>	//B
-#include <string.h>	//A
-
-#define MAX 100	//A
-int counter;	//B
-int sum(int, int);	//B
 
 int main(int argc, char ** argv)
 {
-  int x, y;	//B
-  char name[MAX];	//A
   printf("Hello World\n");
-
-  //B
-  printf("\nEnter x = ");
-  scanf("%d", &x);
-  printf("\nEnter y = ");
-  scanf("%d", &y);
-  printf("\nx + y = %d", sum(x, y));
-
-  return -1;	//B
-}
-
-//B
-int sum(int a, int b)
-{
-  return (a + b);
+  return 0;
 }
