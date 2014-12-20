1e8f3fa1089a2acf01a4fd2d796499579825d755
Vu Tuan Dat <datvtb@bkav.com>
2014-12-20 18:42:58 +0700

    B commits core.c local repo

--- core.c
+++ core.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>	//B
+#include <conio.h>	//B
+
+int counter;	//B
+int sum(int, int);	//B
 
 int main(int argc, char ** argv)
 {
+  int x, y;	//B
   printf("Hello World\n");
-  return 0;
+
+  //B
+  printf("\nEnter x = ");
+  scanf("%d", &x);
+  printf("\nEnter y = ");
+  scanf("%d", &y);
+  printf("\nx + y = %d", sum(x, y));
+
+  return -1;	//B
+}
+
+//B
+int sum(int a, int b)
+{
+  return (a + b);
 }
